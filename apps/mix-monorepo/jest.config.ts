671443c108bb1e9236cd3a38b52cbeb251e17c62
wesley vilas <wesleyvb23@gmail.com>
2025-08-21 08:59:53 -0300

    Initial commit

---
+++ apps/mix-monorepo/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@mix-monorepo/mix-monorepo',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/mix-monorepo',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
